a21d4164322e40005e96be95a29bbeac8fc10c36
Piotr Loos <45639299+piotrloos@users.noreply.github.com>
2020-09-28 11:26:33 +0200

    Added component Board

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import './App.css';
 import Header from './components/Header';
+import Board from './components/Board';
 
 function App() {
   const [score, setScore] = useState(0);
   return (
     <div className="App">
       <Header score={score} />
-      {/* <Board /> */}
+      <Board />
     </div>
   );
 }
